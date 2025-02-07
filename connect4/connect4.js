3909a1c4a6dfdf31556b0e7d78c46b2afd40ebd3
Elliot <elliot.holt@students.mq.edu.au>
2025-02-07 15:40:00 +1100

    Game functions. Tokens fill up the grid and turns alternate.

--- connect4/connect4.js
+++ connect4/connect4.js
@@ -1,5 +1,5 @@
 const repl = require('node:repl');
-const prompt = require('prompt-sync')();
+const prompt = require('prompt-sync')({ sigint: true });
 
 var connectNumber = 4;
 var width = 6;
@@ -18,10 +18,47 @@ function newGame() {
     }
 
     printGridString(grid);
+    ongoingGame(grid);
 }
 
 function ongoingGame(grid) {
+    var playerWins = false;
+    var candidateTurn = true;
+    var token;
 
+    while (!playerWins) {
+        var selectedColumn = "0";
+
+        while (parseInt(selectedColumn) < 1 || parseInt(selectedColumn) > width || isNaN(selectedColumn) || grid[0][selectedColumn - 1] != ".") {
+            switch (candidateTurn) {
+                case true:
+                    selectedColumn = prompt("Candidate's turn. Input a number between 1 and " + width + ": ");
+                    token = "x";
+                    break;
+                case false:
+                    selectedColumn = prompt("Interviewer's turn. Input a number between 1 and " + width + ": ");
+                    token = "o";
+                    break;
+            }
+
+            if (parseInt(selectedColumn) < 1 || parseInt(selectedColumn) > width || isNaN(selectedColumn)) console.log("Invalid input. Try again.");
+            if (grid[0][selectedColumn - 1] != ".") console.log("That column is full. Try again.");
+        }
+
+        for (var i = 0; i < grid.length - 1; i++) {
+            if (grid[i + 1][selectedColumn - 1] != ".") {
+                grid[i][selectedColumn - 1] = token;
+                break;
+            }
+            if (i + 2 == grid.length) {
+                grid[i + 1][selectedColumn - 1] = token;
+                break;
+            }
+        }
+
+        printGridString(grid);
+        candidateTurn = !candidateTurn;
+    }
 }
 
 function printGridString(grid) {
